779a9d97151e2566f5a1889dcd4eb5a15eccd21b
Victorsloop <torres.victor136@gmail.com>
2021-04-17 17:31:09 -0400

    Users controller setup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized
+
+    def index
+        users = User.all.includes(:thoughts)
+        render json: users
+    end 
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end 
+
+    def profile
+        render json: {user: current_user }, status: :accepted
+    end 
+
+    def create
+        @user = User.create!(user_params)
+
+        if @user.valid?
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
+        else 
+            render json: { error: 'Failed to create new account' }, status: :not_acceptable
+        end
+    end 
+
+    def update 
+        @user = User.find(params[:id])
+        if @user.update!(user_params)
+            render json: @user
+        else 
+            render json: {errors: @user.errors}
+        end 
+    end 
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: {}
+    end 
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :password, :email, :name, :pronoun,  :avatar)
+    end 
+
 end
