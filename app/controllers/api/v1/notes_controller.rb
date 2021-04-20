cc272ef566da1164b1b0f73fad38e18536360ce8
Victorsloop <torres.victor136@gmail.com>
2021-04-19 22:18:47 -0400

    Notes controller setup :

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -2,6 +2,28 @@ class Api::V1::NotesController < ApplicationController
     skip_before_action :authorized
 
     def index
-        notes = Notes.all.includes()
+        notes = Note.all
+        render json: notes
     end
+
+    def show 
+        @note = Note.find(params[:id])
+        render json: @note
+    end 
+
+    def create 
+        @note = Note.create!()
+        if @note.valid?
+            render json: @note
+        else 
+            render json: {error: 'Failed to post your note'}, status :not_acceptable
+        end 
+    end 
+
+    def destory
+        @note = Note.find(params[:id])
+        @note.destory!
+        render json {}
+    end 
+
 end
